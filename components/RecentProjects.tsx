60ee83f65e654b0c545f4f89f415f982420e3e5c
Wesley-SdS <wesleysantos.0095@gmail.com>
2024-10-07 21:27:58 -0300

    Updated RecentProjects component to use CSS Grid for responsive layout

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { projects } from '@/data';
+import { PinContainer } from './ui/3d-pin';
 
 const RecentProjects = () => {
   return (
@@ -7,26 +8,32 @@ const RecentProjects = () => {
       <h1 className='heading text-white'>
         <span className='text-purple'> Projects</span>
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 p-4 mt-10">
         {
           projects.map((item) => (
-            <div className='flex flex-col items-center' key={item.id}>
-              <div className="flex flex-col p-4 tracking w-full h-full bg-gray-800 rounded-lg">
-                <h3 className="max-w-xs pb-2 m-0 font-bold text-slate-100 text-center">{item.title}</h3>
-                <span className='text-slate-500'></span>
-                <div className="">
-                  <span className="text-slate-500 text-center">
-                    {item.desc}
-                  </span>
+            <div className='flex flex-col items-center justify-center w-full' key={item.id}>
+              <PinContainer
+                title='google.com.br'
+                href='#'
+              >
+                <div className="flex flex-col p-4 tracking-tight w-[20rem] h-[20rem]">
+                  <h3 className="max-w-xs !pb-2 !m-0 font-bold text-slate-100">
+                    {item.title}
+                  </h3>
+                  <div className="">
+                    <span className="text-slate-500">
+                      {item.desc}
+                    </span>
+                  </div>
+                  <div className="flex flex-1 w-full rounded-lg mt-4">
+                    <img
+                      src={item.img}
+                      alt='cover'
+                      className='z-10 rounded-lg bg-cover'
+                    />
+                  </div>
                 </div>
-                <div className="flex flex-1 w-full rounded-lg mt-4">
-                  <img
-                    src={item.img}
-                    alt='cover'
-                    className='w-full h-auto object-cover rounded-lg'
-                  />
-                </div>
-              </div>
+              </PinContainer>
             </div>
           ))
         }
